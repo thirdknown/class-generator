56a382f85ff00435805a0b0e513a3e2837af07d2
Pavel Simek <pavel.simek@shopsys.com>
2020-03-22 16:30:08 +0100

    generate command: fix help and fix name (due to phpstan)

--- src/Thirdknown/ClassGenerator/GenerateClassesFromJsonCommand.php
+++ src/Thirdknown/ClassGenerator/GenerateClassesFromJsonCommand.php
@@ -11,13 +11,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class GenerateClassesFromJsonCommand extends Command
 {
+    public function __construct()
+    {
+        parent::__construct('generate:classes-from-json');
+    }
+
     protected function configure(): void
     {
-        self::$defaultName = 'generate:classes-from-json';
-
         $this->setDescription('Generates classes from array or JSON.');
 
-        $this->setHelp('php ./console.php generate:classes-from-json generateToDir=temp namespaceName="\\Thirdknown\\GoPay" sourceJsonFile=temp/payment.json');
+        $this->setHelp('php ./console.php generate:classes-from-json temp Thirdknown\\GoPay GoPay temp/payment.json');
 
         $this
             ->addArgument('generateToDir', InputArgument::REQUIRED, 'Where do you want to generate files to?')
